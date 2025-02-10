166dd31d8851d81b56579c2b96b450e0281365b1
Deen <officialdeepinnacle@gmail.com>
2025-02-10 15:58:17 +0100

    added the cta section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaStar } from "react-icons/fa"
+import { FaStar } from "react-icons/fa";
 
 // embla imports
 import useEmblaCarousel from "embla-carousel-react";
@@ -36,7 +36,12 @@ const Agob = () => {
             agriculture, logistics, and more. Building sustainable futures, one
             industry at a time.
           </p>
-          <Link href="" className="bg-light_green w-3/5 h-14 text-lg text-dark_green shadow-2xl shadow-light_green flex items-center justify-center">Get in Touch</Link>
+          <Link
+            href=""
+            className="bg-light_green w-3/5 h-14 text-lg text-dark_green shadow-2xl shadow-light_green flex items-center justify-center"
+          >
+            Get in Touch
+          </Link>
         </div>
         <div className="w-full">
           {/* <div className="bg-red-200 h-6"></div> */}
@@ -141,7 +146,12 @@ const Agob = () => {
                 contributes to building resilient communities and fostering
                 long-term prosperity...{" "}
               </p>
-              <Link href="" className="bg-light_green w-3/5 h-14 text-lg text-dark_green shadow-2xl shadow-light_green flex items-center justify-center">Learn more &rarr;</Link>
+              <Link
+                href=""
+                className="bg-light_green w-3/5 h-14 text-lg text-dark_green shadow-2xl shadow-light_green flex items-center justify-center"
+              >
+                Learn more &rarr;
+              </Link>
             </div>
             <div className="w-full my-4">
               <div className="relative w-full h-96">
@@ -162,89 +172,167 @@ const Agob = () => {
       <section className="w-full">
         <div className="p-2">
           <p className="text-dark_green">What we do</p>
-          <h1 className="text-style text-2xl font-semibold">Our <span className="text-light_green">Services</span></h1>
-          <p className="my-3">Empowering Industries with Integrated, Innovative Solutions</p>
+          <h1 className="text-style text-2xl font-semibold">
+            Our <span className="text-light_green">Services</span>
+          </h1>
+          <p className="my-3">
+            Empowering Industries with Integrated, Innovative Solutions
+          </p>
         </div>
         <MaxWidthWrapper>
           <div className="w-full px-2 ">
-            {
-              services?.map((service)=>(
-                <div key={service.id} className="my-3 pb-6 bg-near_white rounded-md shadow-2xl shadow-light_green/40">
-                  <div className="relative w-full h-64">
-                    <Image src="/construction.jpg" alt="construction" fill priority objectFit="cover" objectPosition="center" />
-                    <div className="overlay"></div>
-                  </div>
-                  <div className="px-2">
-                    <h3 className="text-2xl my-3 text-dark_green">{service.title}</h3>
-                    <p className="text-lg">{service.description}... <span><Link href={`/services/${service.id}`} className="text-dark_green font-semibold">Read more</Link></span> </p>
-                  </div>
-                  {/* <div className="w-full px-2 h-10 flex items-start">
+            {services?.map((service) => (
+              <div
+                key={service.id}
+                className="my-3 pb-6 bg-near_white rounded-md shadow-2xl shadow-light_green/40"
+              >
+                <div className="relative w-full h-64">
+                  <Image
+                    src="/construction.jpg"
+                    alt="construction"
+                    fill
+                    priority
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
+                  <div className="overlay"></div>
+                </div>
+                <div className="px-2">
+                  <h3 className="text-2xl my-3 text-dark_green">
+                    {service.title}
+                  </h3>
+                  <p className="text-lg">
+                    {service.description}...{" "}
+                    <span>
+                      <Link
+                        href={`/services/${service.id}`}
+                        className="text-dark_green font-semibold"
+                      >
+                        Read more
+                      </Link>
+                    </span>{" "}
+                  </p>
+                </div>
+                {/* <div className="w-full px-2 h-10 flex items-start">
                     <Link href={""} className="text-dark_green px-6 py-2 border border-solid border-light_green rounded-full">Read more</Link>
                   </div> */}
-                </div>
-              ))
-            }
+              </div>
+            ))}
           </div>
         </MaxWidthWrapper>
       </section>
       {/* features section */}
-      <section className="w-full mt-3">
+      <section className="w-full mt-6">
         <div className="p-2">
-          <p className="text-dark_green">Why choose <span className="text-light_green">Agob-synergy</span></p>
-          <h1 className="text-style text-2xl font-semibold">Our <span className="text-light_green">Features</span></h1>
+          <p className="text-dark_green">
+            Why choose <span className="text-light_green">Agob-synergy</span>
+          </p>
+          <h1 className="text-style text-2xl font-semibold">
+            Our <span className="text-light_green">Features</span>
+          </h1>
           <p className="my-3">Unifying Excellence Across Industries.</p>
         </div>
         <MaxWidthWrapper>
           <div className="w-full px-2">
-            {
-              features?.map((feature)=>(
-                <div key={ feature.id } className="px-2 py-6 border-t border-solid border-light_green">
-                  <div className="relative w-full h-52">
-                    <Image src={`/${feature.image}`} alt={feature.image} fill priority objectFit="cover" objectPosition="center" />
-                    <div className="overlay"></div>
-                  </div>
-                  <div className="gap-4 my-3">
-                    <span className="font-bold text-light_green text-2xl">0{feature.id}</span>
-                    <span>
-                      <p className="text-xl text-dark_green font-bold mb-2">{feature.title}</p>
-                      <p className="text-lg">{feature.description}</p>
-                    </span>
-                  </div>
+            {features?.map((feature) => (
+              <div
+                key={feature.id}
+                className="px-2 py-6 border-t border-solid border-light_green"
+              >
+                <div className="relative w-full h-52">
+                  <Image
+                    src={`/${feature.image}`}
+                    alt={feature.image}
+                    fill
+                    priority
+                    objectFit="cover"
+                    objectPosition="center"
+                  />
+                  <div className="overlay"></div>
                 </div>
-              ))
-            }
+                <div className="gap-4 my-3">
+                  <span className="font-bold text-light_green text-2xl">
+                    0{feature.id}
+                  </span>
+                  <span>
+                    <p className="text-xl text-dark_green font-bold mb-2">
+                      {feature.title}
+                    </p>
+                    <p className="text-lg">{feature.description}</p>
+                  </span>
+                </div>
+              </div>
+            ))}
           </div>
         </MaxWidthWrapper>
       </section>
       {/* testimonial section */}
       <section className="w-full">
         <div className="p-2">
-          <p className="text-dark_green">Voice of<span className="text-light_green">Trust</span></p>
-          <h1 className="text-style text-2xl font-semibold">What Our<span className="text-light_green">Clients</span> Are Saying</h1>
-          <p className="my-3">Our clients and partners speak for us. Learn how our commitment to excellence and innovation has made a lasting impact across multiple sectors.</p>
+          <p className="text-dark_green">
+            Voice of<span className="text-light_green">Trust</span>
+          </p>
+          <h1 className="text-style text-2xl font-semibold">
+            What Our<span className="text-light_green">Clients</span> Are Saying
+          </h1>
+          <p className="my-3">
+            Our clients and partners speak for us. Learn how our commitment to
+            excellence and innovation has made a lasting impact across multiple
+            sectors.
+          </p>
         </div>
         <MaxWidthWrapper>
           <div className="w-full">
-            {
-              testimonials?.map((value)=>(
-                <div key={value.id} className="w-full px-2 my-3 flex flex-col items-center justify-center">
-                  <p className="text-center text-dark_green text-xl my-3">{value.company}</p>
-                  <p className="text-center text-lg">{value.testimonial}</p>
-                  <span className="flex flex-row items-center gap-2 mt-4 mb-2 text-light_green text-xl">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                  </span>
-                  <p className="text-center text-lg font-bold">{value.name}</p>
-                  <p className="text-center text-lg font-semibold">{value.position}</p>
-                </div>
-              ))
-            }
+            {testimonials?.map((value) => (
+              <div
+                key={value.id}
+                className="w-full px-2 my-3 flex flex-col items-center justify-center"
+              >
+                <p className="text-center text-dark_green text-xl my-3">
+                  {value.company}
+                </p>
+                <p className="text-center text-lg">{value.testimonial}</p>
+                <span className="flex flex-row items-center gap-2 mt-4 mb-2 text-light_green text-xl">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                </span>
+                <p className="text-center text-lg font-bold">{value.name}</p>
+                <p className="text-center text-lg font-semibold">
+                  {value.position}
+                </p>
+              </div>
+            ))}
           </div>
         </MaxWidthWrapper>
       </section>
+      {/* cta section */}
+      <div className="w-full px-2">
+        <div className="w-full bg-light_green">
+          <div className="w-full h-96 bg-red-400 relative">
+            <Image
+              src={"/workers.jpg"}
+              alt="agob-logo"
+              fill
+              priority
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+          <div className="w-full h-96 bg-red-400 relative -top-36">
+            <Image
+              src={"/agob-logo.jpg"}
+              alt="agob-logo"
+              fill
+              priority
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
